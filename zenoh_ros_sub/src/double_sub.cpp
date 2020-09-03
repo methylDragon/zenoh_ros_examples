0114e8153428b1187c7686839dc8282e9ae13ea4
methylDragon <methylDragon@gmail.com>
2020-09-03 14:36:23 +0800

    Implement double sub

--- zenoh_ros_sub/src/double_sub.cpp
+++ zenoh_ros_sub/src/double_sub.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2016 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,50 +12,46 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <chrono>
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "rclcpp/subscription_options.hpp"
-
 #include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
 
-class MinimalSubscriberWithTopicStatistics : public rclcpp::Node
+class MinimalSubscriber : public rclcpp::Node
 {
 public:
-  MinimalSubscriberWithTopicStatistics()
-  : Node("minimal_subscriber_with_topic_statistics")
+  MinimalSubscriber()
+  : Node("wow")
   {
-    // manually enable topic statistics via options
-    auto options = rclcpp::SubscriptionOptions();
-    options.topic_stats_options.state = rclcpp::TopicStatisticsState::Enable;
-
-    // configure the collection window and publish period (default 1s)
-    options.topic_stats_options.publish_period = std::chrono::seconds(10);
-
-    // configure the topic name (default '/statistics')
-    // options.topic_stats_options.publish_topic = "/topic_statistics"
-
-    auto callback = [this](std_msgs::msg::String::SharedPtr msg) {
-        this->topic_callback(msg);
-      };
-
     subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, callback, options);
+      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+
+    duplicate_subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MinimalSubscriber::duplicate_topic_callback, this, _1));
   }
 
 private:
   void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    fflush(stdout);
   }
+
+  void duplicate_topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I ALSO heard: '%s'", msg->data.c_str());
+    fflush(stdout);
+  }
+
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr duplicate_subscription_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriberWithTopicStatistics>());
+  rclcpp::spin(std::make_shared<MinimalSubscriber>());
   rclcpp::shutdown();
   return 0;
 }
